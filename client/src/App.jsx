3b450e51a294d7330332c598b2cf8d2104768c93
niveyrojulian <niveyrojulian5@gmail.com>
2024-07-26 03:30:23 -0300

    Arreglado problema de enrutamiento al parecer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { authenticateUserFromSession } from "./redux/actions/actions";
 import Error from "./pages/Error";
-import { isatty } from "tty";
 
 function App() {
   const dispatch = useDispatch();
@@ -17,21 +16,20 @@ function App() {
     dispatch(authenticateUserFromSession());
   }, [dispatch]);
 
-  console.log(isAuth);
-
   return (
     <div>
       <Toaster />
       <Routes>
-        <Route path="/" element={<Login />}></Route>
+        <Route path="/" element={<Login />} />
         {isAuth ? (
           <>
-            <Route path="/dashboard" element={<Dashboard />}></Route>
-            <Route path="/products" element={<Products />}></Route>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/products" element={<Products />} />
           </>
         ) : (
-          <Route path="/error" element={<Error />}></Route>
+          <Route path="/error" element={<Error />} />
         )}
+        <Route path="*" element={<Error />} />
       </Routes>
     </div>
   );
