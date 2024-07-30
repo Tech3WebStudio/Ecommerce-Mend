7250494c98107446b0528207904c065adaf1558b
niveyrojulian <niveyrojulian5@gmail.com>
2024-07-30 19:07:20 -0300

    cambios en app.js cambiando el server. listen

--- server/src/app.js
+++ server/src/app.js
@@ -37,9 +37,4 @@ server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
 module.exports = server;
