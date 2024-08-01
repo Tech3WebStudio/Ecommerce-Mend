fb1da964274dc42da9247300daa46118945b23f1
niveyrojulian <niveyrojulian5@gmail.com>
2024-07-31 23:46:48 -0300

    Cambiando configuracion de index.js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,6 @@
 const server = require("./src/app"); 
 const PORT = process.env.PORT || 3000;
 
-<<<<<<< HEAD
-=======
-// Sirve los archivos estáticos de la carpeta client/build
-server.use(express.static(path.join(__dirname, "../client/build")));
-
-server.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-});
->>>>>>> 7a60427 (Update index.js)
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
