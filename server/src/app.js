8ea8fbedad7b51778cfed80aaffccdf6bf7d3814
Julian Gabriel Niveyro <115017654+NiveJulian@users.noreply.github.com>
2024-07-31 22:34:09 -0300

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -15,7 +15,11 @@ server.use(express.json());
 server.use(cookieParser());
 server.use(
   cors({
-    origin: ["https://ecommerce-mend.onrender.com", "http://localhost:5173"],
+    origin: [
+      "https://ecommerce-mend.onrender.com",
+      "https://nina-ecommerce-roan.vercel.app",
+      "http://localhost:5173"
+    ],
     credentials: true,
   })
 );
