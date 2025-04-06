f655c07fd8f556d4388800cb1205d0f8b8fdc66d
NahuelArg <claudioarganaraz86@gmail.com>
2025-04-06 14:29:04 -0300

    update branch from dev

--- client/src/pages/ecommerce/ProductDetail.jsx
+++ client/src/pages/ecommerce/ProductDetail.jsx
@@ -143,6 +143,7 @@ const ProductDetail = () => {
               >
                 {"Agregar al carrito"}
               </button>
+              <button class="flex gap-2 border border-gray-400 p-2 active:translate-y-[1px] hover:shadow-lg rounded-md"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3"></path></svg><span>Volver</span></button>
             </div>
           </div>
         </div>
